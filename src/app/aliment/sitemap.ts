5e4a024b00ee87626eb1480a4a6467de75d93c17
brahima <brahimafofana121@gmail.com>
2025-12-09 20:14:43 +0000

    Amelioration

--- src/app/aliment/sitemap.ts
+++ src/app/aliment/sitemap.ts
@@ -3,10 +3,9 @@ import { BASE_URL } from '@/config/constantes'
 import { foods } from '@/data/data'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-return foods.map(food => ({
-  url: `${BASE_URL}/aliment/${food.name}`,
-  lastModified: new Date(),
-  priority: 0.7
-}))
-
+  return foods.map(food => ({
+    url: `${BASE_URL}/aliment/${food.name}`,
+    lastModified: new Date(),
+    priority: 0.7
+  }))
 }
