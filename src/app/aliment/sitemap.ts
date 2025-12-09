2b83728f8511ec0280a7db6c931c7623c52b39d4
brahima <brahimafofana121@gmail.com>
2025-12-09 20:00:42 +0000

    Amelioration

---
+++ src/app/aliment/sitemap.ts
@@ -0,0 +1,12 @@
+import { MetadataRoute } from 'next'
+import { BASE_URL } from '@/config/constantes'
+import { foods } from '@/data/data'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+return foods.map(food => ({
+  url: `${BASE_URL}/aliment/${food.name}`,
+  lastModified: new Date(),
+  priority: 0.7
+}))
+
+}
---
+++ src/app/sitemap.ts
@@ -0,0 +1,12 @@
+import { MetadataRoute } from 'next'
+import { BASE_URL } from '@/config/constantes'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: BASE_URL,
+      lastModified: new Date(),
+        priority: 1
+    },
+  ]
+}
